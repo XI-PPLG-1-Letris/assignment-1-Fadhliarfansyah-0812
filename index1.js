3dc1171bbfbfd6e50c9ff8bfb6cf4de9ee3b7191
Fadhliarfansyah-0812 <arfanflows@gmail.com>
2025-10-05 21:58:08 +0700

    done pak

---
+++ index1.js
@@ -0,0 +1,23 @@
+//- Mini Calculator
+
+//variabel
+let a = 10;
+let b = 20;
+
+// Arrow functions
+const tambah = (x, y) => x + y;
+const kurang = (x, y) => x - y;
+const kali = (x, y) => x * y;
+const bagi = (x, y) => x / y;
+const modulus = (x, y) => x % y;
+
+// Tampilkan nilai awal
+console.log(`a = ${a}`);
+console.log(`b = ${b}\n`);
+
+// Tampilan hasil operasi aritmatika
+console.log(`tambah(a, b)\nlog : Penjumlahan ${a} + ${b} = ${tambah(a, b)}\n`);
+console.log(`kurang(a, b)\nlog : Pengurangan ${a} - ${b} = ${kurang(a, b)}\n`);
+console.log(`kali(a, b)\nlog : Perkalian ${a} x ${b} = ${kali(a, b)}\n`);
+console.log(`bagi(a, b)\nlog : Pembagian ${a} / ${b} = ${bagi(a, b)}\n`);
+console.log(`modulus(a, b)\nlog : Modulus ${a} % ${b} = ${modulus(a, b)}\n`);
